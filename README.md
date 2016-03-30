546ca01c84b1d59842e36789979efdaaf68bea81
bhattigurjot <bhattigurjot@gmail.com>
2016-03-30 22:30:55 +0530

    Add basic test and output handler

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lua-busted
 Lua unit testing with busted
+
+# RUN as:
+lua test.lua -o output_handlers/output_handler_TAP.lua
---
+++ test.lua
@@ -0,0 +1,28 @@
+require 'busted.runner'()
+
+function add(v1,v2)
+    -- add positive numbers
+    -- return 0 if any of the numbers are 0
+    -- error if any of the two numbers are negative
+    if v1 < 0 or v2 < 0 then
+        error('Can only add positive or null numbers, received '..v1..' and '..v2)
+    end
+    if v1 == 0 or v2 == 0 then
+        return 0
+    end
+    return v1+v2
+end
+
+describe("Test", function()
+    -- tests
+    describe("Add", function()
+        it("Positive", function()
+        assert.are.same(add(1,1),2)
+        end)
+        it("Zero", function()
+        assert.are.same(add(1,0),0)
+        assert.are.same(add(0,5),0)
+        assert.are.same(add(0,0),0)
+        end)
+    end)
+end)
